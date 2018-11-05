2fad00eec9f0926cbf76003ca87bdc9935be27b4
abdalhafez1997 <abdalhafezsayeh1997@gmail.com>
2018-11-05 18:56:40 +0200

    first commit

---
+++ New folder/js/Text.js
@@ -0,0 +1,13 @@
+
+var
+   price = 2000,
+   product1 = 50,
+   product2 = 100,
+   product3 = 150,
+   product4 = 200;
+   
+document.getElementById("price").innerHTML = price;
+document.getElementById("product1").innerHTML = price - product1;
+document.getElementById("product2").innerHTML = price - product2;
+document.getElementById("product3").innerHTML = price - product3;
+document.getElementById("product4").innerHTML = price - product4;
---
+++ js/js/Untitled.js
@@ -0,0 +1,10 @@
+
+
+var myOldPrice = 2000,
+ 
+    myNewPrice = 1000,
+
+    myDiscound = myOldPrice - myNewPrice ;
+
+
+document.getElementById("price").innerHTML = myDiscound;
